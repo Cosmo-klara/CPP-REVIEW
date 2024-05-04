022698e4775acc9d92e18158f4158bfb6983254a
Cosmo Clara <wincosmo9@outlook.com144621976+Cosmo-klara@users.noreply.github.com>
2024-05-04 16:29:22 +0800

    new file:   9.md 	new file:   code/9/9-1.cpp 	new file:   code/9/9-2.cpp 	new file:   img/9/9-1.png 	new file:   img/9/9-2.png 	new file:   img/9/9-3.png 	new file:   img/9/9-4.png

---
+++ code/9/9-1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+#define BAND(x) (((x) > 5 && (x) < 10) ? (x) : 0)
+
+int main()
+{
+  for (int i = 4; i < 11; i++)
+  {
+    int a = i;
+    cout << "a = " << a << endl
+         << '\t';
+    cout << "BAND(++a)=" << BAND(++a) << endl;
+    cout << "\t a = " << a << endl;
+  }
+}
