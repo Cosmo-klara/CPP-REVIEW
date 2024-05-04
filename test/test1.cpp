d4d3b57a920b3e2ec0e3b181464aef5572e36617
Cosmo Clara <wincosmo9@outlook.com144621976+Cosmo-klara@users.noreply.github.com>
2024-05-05 01:26:25 +0800

    modified:   12.md 	new file:   test/test1.cpp

---
+++ test/test1.cpp
@@ -0,0 +1,10 @@
+// Define a class,Point,with demands as follows:
+// (1)The class has member data X and Y which describe a point's coordinates.
+// (2)Define a member function Distance to get the distance()between two points.
+// (3)Define a friend function Distance to get the distance between two points.
+// (4)A programmer can use Point in main()as follows:
+// void main()
+// Point p,q(2,3); // The coordinate of point p is original point.
+// cout <p.Distance(q)<<endl;
+// cout <Distance(p,q)<<endl;
+
